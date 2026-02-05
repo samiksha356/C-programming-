0f3b902e318f27fe1a71eddf54be7f5efe2bc151
samiksha356 <2025a6r051@mietjammu.in>
2026-02-05 23:37:06 +0530

    Add square star pattern generator in C

---
+++ Square Star Pattern Generator!.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++){
+         for(int i=1; i<=n; i++){
+             printf("* ");
+         }
+        printf("\n");
+    }
+    
+    
+    return 0;
+}
