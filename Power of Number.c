15715e8881382fc10598f899a39ab25cf9acecbc
samiksha356 <2025a6r051@mietjammu.in>
2026-02-05 22:36:19 +0530

    Add program to calculate power of a number

---
+++ Power of Number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int x, a, b;
+    scanf("%d %d", &a, &b);
+    x = pow(a,b);
+    printf("%d", x);
+    return 0;
+}
