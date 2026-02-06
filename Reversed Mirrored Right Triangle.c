1cec978879119315eb0466b350d715613e3daa81
samiksha356 <2025a6r051@mietjammu.in>
2026-02-06 18:42:05 +0530

    Add Reversed Mirrored Right Triangle program

---
+++ Reversed Mirrored Right Triangle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    
+
+
+    int N;
+    scanf("%d", &N); 
+
+    for (int i = 1; i <= N; i++) {         
+        for (int j = 1; j <= N - i; j++) { 
+            printf("  ");                  
+        }
+        for (int k = 1; k <= i; k++) {     
+            printf("* ");
+        }
+        printf("\n");                      
+    }
+
+    return 0;
+}    
+    
