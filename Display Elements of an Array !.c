52d3ebb9b0143e2bd66a19f12a931d2ffdc21f17
samiksha356 <2025a6r051@mietjammu.in>
2026-02-06 18:45:16 +0530

    Create Display Elements of an Array !.c

---
+++ Display Elements of an Array !.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    
+    
+    int N;
+    scanf("%d", &N); 
+    
+    int arr[N];       
+    
+    
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    
+    for (int i = 0; i < N; i++) {
+        printf("%d", arr[i]);
+        if (i < N - 1) {
+            printf(" "); 
+        }
+    }
+
+    return 0;
+}
+    
